89c35bc9fc735259afeda37765242ec40981d722
Taylor Radtke <tradtke@aapa.org>
2017-11-28 09:10:57 -0500

    init

---
+++ Gruntfile.js
@@ -0,0 +1,175 @@
+const pixRem = require('pixrem');
+const postcssFlexboxfixer = require('postcss-flexboxfixer');
+const autoPrefixer = require('autoprefixer');
+
+module.exports = grunt => {
+	grunt.initConfig({
+		// pug task
+		pug: {
+			compile: {
+				options: {
+					data: {
+						debug: true,
+					},
+					pretty: true,
+				},
+				files: [
+					{
+						src: '*.pug',
+						cwd: 'src/pug/',
+						dest: 'dist',
+						expand: true,
+						ext: '.html',
+					},
+				],
+			},
+		},
+
+		// sass task
+		sass: {
+			dist: {
+				options: {
+					style: 'inline',
+				},
+				files: [
+					{
+						src: '*.scss',
+						cwd: 'src/sass/',
+						dest: 'dist/css',
+						expand: true,
+						ext: '.css',
+					},
+				],
+			},
+		},
+
+		postcss: {
+			options: {
+				map: true,
+				processors: [
+					pixRem(), // add fallbacks for rem units
+					postcssFlexboxfixer,
+					autoPrefixer({ browsers: ['last 2 versions', 'ie >= 9'] }), // add vendor prefixes
+				],
+			},
+			dist: {
+				src: 'dist/css/application.css',
+			},
+		},
+
+		// babel
+		babel: {
+			options: {
+				sourceMap: false,
+				presets: ['env'],
+			},
+			dist: {
+				files: {
+					'src/js/compiled/app.js': 'src/js/app.js',
+				},
+			},
+		},
+
+		// uglify task (js minify)
+		uglify: {
+			my_target: {
+				files: {
+					'dist/js/app.min.js': ['src/js/compiled/app.js'],
+				},
+			},
+		},
+
+		// copy task (copy src/libraries to dist/libraries)
+		copy: {
+			main: {
+				expand: true,
+				cwd: 'src',
+				src: ['libraries/**'],
+				dest: 'dist/',
+			},
+		},
+
+		// image compress task (compress all image src/images to dist/images)
+		imagemin: {
+			dynamic: {
+				files: [
+					{
+						expand: true,
+						cwd: 'src/',
+						src: ['images/**/*.{png,jpg,gif}'],
+						dest: 'dist',
+					},
+				],
+			},
+		},
+		// minify css (only tun in production)
+		cssmin: {
+			target: {
+				files: [
+					{
+						expand: true,
+						cwd: 'dist/css',
+						src: ['*.css', '!*.min.css'],
+						dest: 'dist/css',
+						ext: '.css',
+					},
+				],
+			},
+		},
+		// auto refresh view on change in dist directory
+		browserSync: {
+			dev: {
+				bsFiles: {
+					src: ['dist/**.*'],
+				},
+				options: {
+					watchTask: true,
+					server: './dist',
+				},
+			},
+		},
+
+		// watch change inside directory to run task
+		watch: {
+			pug: {
+				files: ['src/pug/**/*.pug'],
+				tasks: ['pug'],
+			},
+			sass: {
+				files: ['src/sass/**/*.scss'],
+				tasks: ['sass', 'postcss'],
+			},
+			js: {
+				files: ['src/js/**/*.js'],
+				tasks: ['babel', 'uglify'],
+			},
+			copy: {
+				files: ['src/libraries/**'],
+				tasks: ['copy'],
+			},
+			imagemin: {
+				files: ['src/images/**'],
+				tasks: ['imagemin'],
+			},
+		},
+	});
+
+	// initial
+	grunt.loadNpmTasks('grunt-babel');
+	grunt.loadNpmTasks('grunt-browser-sync');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-pug');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-postcss');
+
+	// register default task
+	if (process.env.NODE_ENV === 'production') {
+		grunt.registerTask('default', ['pug', 'sass', 'babel', 'uglify', 'copy', 'imagemin', 'cssmin']);
+	} else {
+		grunt.registerTask('default', ['pug', 'sass', 'babel', 'uglify', 'copy', 'imagemin', 'browserSync', 'watch']);
+	}
+};
