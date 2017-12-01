31249608b39a9235bb4877a8b369a94ab4c81088
Taylor Radtke <tradtke@aapa.org>
2017-11-30 19:11:01 -0500

    added page-titles

--- src/js/compiled/app.js
+++ src/js/compiled/app.js
@@ -161,3 +161,48 @@ $('.js-vertical-tab-accordion-heading').click(function (e) {
 	$('.js-vertical-tab').removeClass('is-active');
 	$('.js-vertical-tab[rel^=\'' + accordionActiveTab + '\']').addClass('is-active');
 });
+
+/* ---------------------------------------------
+ Countdown Clock
+ --------------------------------------------- */
+// function getTimeRemaining(endtime) {
+// 	const t = Date.parse(endtime) - Date.parse(new Date());
+// 	const seconds = Math.floor((t / 1000) % 60);
+// 	const minutes = Math.floor((t / 1000 / 60) % 60);
+// 	const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+// 	const days = Math.floor(t / (1000 * 60 * 60 * 24));
+// 	return {
+// 		total: t,
+// 		days,
+// 		hours,
+// 		minutes,
+// 		seconds,
+// 	};
+// }
+
+// function initializeClock(id, endtime) {
+// 	const clock = document.querySelector(id);
+// 	const daysSpan = clock.querySelector('.days');
+// 	const hoursSpan = clock.querySelector('.hours');
+// 	const minutesSpan = clock.querySelector('.minutes');
+// 	const secondsSpan = clock.querySelector('.seconds');
+// 	let countdownInterval;
+
+// 	function updateClock() {
+// 		const t = getTimeRemaining(endtime);
+
+// 		daysSpan.innerHTML = t.days;
+// 		hoursSpan.innerHTML = `0${t.hours}`.slice(-2);
+// 		minutesSpan.innerHTML = `0${t.minutes}`.slice(-2);
+// 		secondsSpan.innerHTML = `0${t.seconds}`.slice(-2);
+
+// 		if (t.total <= 0) {
+// 			clearInterval(countdownInterval);
+// 		}
+// 	}
+
+// 	updateClock();
+// 	countdownInterval = setInterval(updateClock, 1000);
+// }
+
+// initializeClock('.js-countdown', new Date(2017, 12, 27));
